9a6d725aa52b7d44bb774dd254f684ed6cf3552a
Jovan Stojimirovic <jovan@Jovans-MacBook-Pro.local>
2020-07-28 22:38:10 +0200

    .

--- src/server/object/search/index.js
+++ src/server/object/search/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import elastic from '@elastic/elasticsearch'
+const client = new elastic.Client({ node: 'http://localhost:9200' })
 const router = express.Router()
 
 router.post( "/", (req, res) => {
-    
+    client.search({
+        index: req.auth.uid,
+        body: {
+            query: {
+                match: {
+                    quote: req.query.search
+                }
+            }
+        }
+    })
 })
 
 export default router
--- src/server/object/upload/index.js
+++ src/server/object/upload/index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import elastic from '@elastic/elasticsearch'
+import lodash from 'lodash'
+const client = new elastic.Client({ node: 'http://88.99.54.174:9200' })
 const router = express.Router()
 
 
@@ -9,16 +12,20 @@ import minioClient from '../../../minio/connection'
 import { user, object } from '../../../database/schema'
 var upload = multer({ dest: '/tmp' })
 
+const cocoSsd = require('@tensorflow-models/coco-ssd');
+const tf = require('@tensorflow/tfjs-node');
+const fs = require('fs').promises;
 
 
-router.post( "/*", upload.single( 'upload' ), (req, res) => {
+
+router.post("/*", upload.single('upload'), (req, res) => {
     console.log(req._parsedUrl)
     const objectName = uuidv4()
     console.log(req.file)
     minioClient.fPutObject(String(req.auth.uid).toLowerCase(), objectName, req.file.path, ({
         'Content-Type': req.file.mimetype,
     }), (err) => {
-        if(err){
+        if (err) {
             console.log(err)
             res.status(503)
             res.json({
@@ -26,11 +33,11 @@ router.post( "/*", upload.single( 'upload' ), (req, res) => {
                 upload: false
             })
         }
-        else{
+        else {
             user.findOne({
                 uid: req.auth.uid
             }).then((result) => {
-                if(result){
+                if (result) {
                     object.create({
                         type: false,
                         file_type: req.file.mimetype,
@@ -40,15 +47,55 @@ router.post( "/*", upload.single( 'upload' ), (req, res) => {
                         object_user_name: req.file.originalname,
                         owner_uid: result._id
                     }).then((onfullfilled, onrejected) => {
-                        if(onfullfilled){
-                            console.log(onfullfilled)
+                        if (onfullfilled) {
+
                             res.json({
                                 status: true,
                                 upload: true,
                                 database: true
                             })
+                            if (req.file.mimetype.startsWith("image")) {
+                                Promise.all([cocoSsd.load(), fs.readFile(req.file.path)])
+                                    .then((results) => {
+                                        // First result is the COCO-SSD model object.
+                                        const model = results[0];
+                                        // Second result is image buffer.
+                                        const imgTensor = tf.node.decodeImage(new Uint8Array(results[1]), 3);
+                                        // Call detect() to run inference.
+                                        return model.detect(imgTensor);
+                                    })
+                                    .then((predictions) => {
+                                        let results = predictions
+                                        let resultValues = []
+                                        for (let value of results) {
+                                            resultValues.push(value.class)
+                                        }
+                                        let count = {};
+                                        resultValues.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
+                                        console.log(resultValues)
+                                        console.log(count)
+                                        let countString = ""
+                                        for (let i in count) {
+                                            countString += count[i] + " " + i + " "
+                                        }
+                                        console.log(countString)
+                                        client.index({
+                                            index: req.auth.uid,
+                                            body: {
+                                                search: countString
+                                            }
+                                        }).then((onfullfilled, onrejected) => {
+                                            if(onfullfilled){
+                                                
+                                            }
+                                            if(onrejected){
+                                                
+                                            }
+                                        })
+                                    })
+                            }
                         }
-                        if(onrejected){
+                        if (onrejected) {
                             res.status(503)
                             res.json({
                                 status: false,
