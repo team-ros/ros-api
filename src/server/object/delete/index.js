013efc11847dda5aec7a63606c68a02fcb734758
Jovan Stojimirovic <jovan@Jovans-MacBook-Pro.local>
2020-08-02 21:04:30 +0200

    move

--- src/server/object/delete/index.js
+++ src/server/object/delete/index.js
@@ -1,8 +1,13 @@
 import express from 'express'
+import { user, object } from '../../../database/schema'
 const router = express.Router()
 
 router.post( "/", (req, res) => {
-    
+    user.findOne({
+        uid: req.auth.uid
+    }).then((result) => {
+        
+    })
 })
 
 export default router
--- src/server/object/move/index.js
+++ src/server/object/move/index.js
@@ -14,24 +14,53 @@ router.post("/", (req, res) => {
             object_uuid: req.body.Object_ID
         }).then((folderResult) => {
             let oldPath = folderResult.object_user_path
+            const oldName = folderResult.object_user_name
+            console.log(oldName)
             let check = new RegExp("^(/[^/ ]*)+/?$")
             if (check.test(req.body.Path)) {
-                object.update({
+                object.updateOne({
                     object_uuid: req.body.Object_ID
                 },
                     {
                         object_user_path: req.body.Path,
-                        object_user_name: req.body.newName
+                        object_user_name: req.body.Name
                     }).then((onfulfilled, onrejected) => {
                         if (onfulfilled) {
-                            object.update({
+                            object.find({
                                 object_user_path: {
-                                    $regex: new RegExp(req.body.Path + ".*")
+                                    $regex: oldPath + ".*"
                                 }
-                            })
-                            res.json({
-                                status: true,
-                                move: true
+                            }).then((ObjectToUpdate) => {
+                                console.log(ObjectToUpdate)
+                                for (let i of ObjectToUpdate) {
+                                    let fullPath = i.object_user_path
+                                    let splittedPath = fullPath.split("/")
+  
+                                    if (splittedPath.includes(oldName)) {
+                                        splittedPath[splittedPath.indexOf(oldName)] = req.body.Name
+                                    }
+
+                                    fullPath = splittedPath.join("/")
+
+                                    object.updateOne( i , {
+                                        object_user_path: fullPath
+                                    }).then((onfulfilled, onrejected) => {
+                                        if (onfulfilled) {
+                                            res.json({
+                                                status: true,
+                                                move: true
+                                            })
+                                        }
+                                        if (onrejected) {
+                                            res.status(500)
+                                            res.json({
+                                                status: false,
+                                                move: false
+                                            })
+                                        }
+                                    })
+
+                                }
                             })
                         }
                         if (onrejected) {
